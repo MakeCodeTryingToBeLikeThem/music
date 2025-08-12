f958627d4fabbddc91bc7e5cfe825df600d40c39
MakeCodeTryingToBeLikeThem <makecodedevteam@gmail.com>
2025-08-12 15:29:00 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "mySong":return hex`0078000408020100001c00010a006400f401640000040000000000000000000000000005000004480004000800012208000c0001220c001000012410001400012218001c0001201c002000012220002400012424002800012528002c0001272c003000012930003400012a340038000129`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
